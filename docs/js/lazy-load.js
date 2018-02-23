0b37dd6b8da1686c798723ff222711da5b9200ed
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-02-23 15:27:39 +0200

    Improve lazy-load.js. Check if the browser supports IntersectionObserver. If not, load all images immediately. Add .fade-in class to images in order to add a fade-in effect on loading. Update docs.

--- docs/js/lazy-load.js
+++ docs/js/lazy-load.js
@@ -1,39 +1,88 @@
-// Target images
-var images = document.querySelectorAll('.js-lazy-load');
+// Author: PictureElement (Marios Sofokleous)
+// Description: Avoid costly scroll andlers and lazy load images using
+// Intersection Observer.
+// Date: 23.02.17
 
-// Options
-var options = {
-  rootMargin: '50px 0px',
-  threshold: 0.01
+// ------------------------------ HELPER FUNCTION ------------------------------
+
+// Load image
+function applyImage (image, source) {
+  // Prevent this from being lazy loaded a second time
+  image.classList.add('js-lazy-load--handled');
+  // Load image
+  image.src = source;
+  // Fade-in image load
+  image.classList.add('fade-in');
 }
 
-// Create an intersection observer
-var io = new IntersectionObserver(callback, options);
+// Preload image in browser's cache
+function preloadImage (image) {
+  // Get url from data-src attribute
+  var url = image.dataset.src;
+  if (!url) {
+    return;
+  }
+  // Built-in image object
+  var img = new Image();
+  // Preload image
+  img.src = url;
+  // Apply image
+  applyImage(image, url);
+}
 
-// Start observing the images
-images.forEach(function (image) {
-  io.observe(image);
-});
-
-// Callback
+// Intersection observer's callback
 function callback (entries, observer) {
   entries.forEach(function (entry) {
     if (entry.intersectionRatio > 0) {
+      // Stop observing target
       observer.unobserve(entry.target);
-      loadImage(entry.target);
+      // Preload target in browser's cache
+      preloadImage(entry.target);
     }
   })
+  // Disconnect the observer
+  observer.disconnect();
 }
 
-function loadImage (image) {
-  // Get data-src attribute
-  var src = image.dataset.src;
-  console.log('TEST:' + src);
-  // fetch image
-  fetch(src).then(function (response) {
-    // Prevent image from being lazy loaded a second time
-    image.classList.remove('js-lazy-load');
-    // Set src attribute
-    image.src = src;
-  })
+// Load images immediately if intersection observer is not supported by the
+// browser
+function loadImagesImmediately (images) {
+  images.forEach(function (image) {
+    preloadImage(image);
+  });
+}
+
+// --------------------------------- MAIN LOGIC --------------------------------
+
+// Return a list of all elements within the document with the specified class
+var images = document.querySelectorAll('.js-lazy-load');
+
+// If the browser supports IntersectionObserver
+if ('IntersectionObserver' in window) {
+  // Options
+  var options = {
+    // Grow each side of the root element's (viewport) bounding box. For
+    // example, the target will be downloaded once intersects the area above or
+    // below the viewport
+    rootMargin: '100px 0px',
+    // A threshold of 1.0 means that when 100% of the target is visible within
+    // the element specified by the root option (if the root is not specified it
+    // defaults to the browser viewport), the callback is invoked.
+    threshold: 0.01
+  }
+
+  // Create an intersection observer
+  var io = new IntersectionObserver(callback, options);
+
+  // Start observing the images
+  images.forEach(function (image) {
+    // Do not observe images that have been loaded
+    if (image.classList.contains('js-lazy-load--handled')) {
+      return;
+    }
+    // Observe image
+    io.observe(image);
+  });
+} else {
+  loadImagesImmediately(images);
 }
